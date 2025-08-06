8356d096a8369750694c66034b685607579a685b
Areesha77 <areeshazainab77@gmail.com>
2025-08-07 01:08:01 +0500

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,6 @@
+// chatbot.js
+// This script is part of the GAO Tek Healthcare Chatbot project.
+// The chatbot itself is powered by Botpress Cloud and embedded via HTML script tags.
+// This file is included for structure and documentation purposes only.
+
+console.log("GAO Tek Healthcare Chatbot loaded.");
